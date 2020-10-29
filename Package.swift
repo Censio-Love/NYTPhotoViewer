f5e58b91271c4cb2ca9518630be58efc9d6269e2
Wellington Moreno <jwellington.moreno@gmail.com>
2020-10-28 18:41:29 -0700

    Adds SPM support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NYTPhotoViewer",
+    platforms: [
+             .macOS(.v10_10),
+             .iOS(.v9),
+             .tvOS(.v9)
+         ],
+    products: [
+        .library(
+            name: "NYTPhotoViewer",
+            type: .static,
+            targets: ["NYTPhotoViewer"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pinterest/PINRemoteImage.git", from: "3.0.2"),
+    ],
+    targets: [
+        .target(
+            name: "NYTPhotoViewer",
+            dependencies: ["PINRemoteImage"],
+            path: "NYTPhotoViewer",
+            publicHeadersPath: "include"
+        )
+    ]
+)
